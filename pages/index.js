fc68ac0c8d6e643cad615784cdecb1b75236c689
Christof <userpattern@gmail.com>
2024-09-11 09:53:57 +0200

    add function to genearte arra for 4x4 square, basic styling

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import { useEffect, useState } from "react";
+import { initialState } from "@/initialData";
 
 
 const ProgressBar = styled.div`
@@ -36,20 +37,39 @@ const SquarrelTitle = styled.h1`
   margin:  2rem auto 2rem;
 `;
 
-const MainFlexContainer = styled.div`
+const TestFlexContainer = styled.div`
   display: flex;
-
   flex-direction: column;
   padding: 0.5rem;
   min-height: 400px;
-  height: 100%;
-  width: 100%;
+  height: 90%;
+  width: 90%;
   align-content: center;
   align-items: center;
+  border: 2px solid black;
+ 
+`;
+const ButtonContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  padding: 0.5rem;
+  margin: auto; 
+  min-height: 40px;
+  width: 50%;
+  align-content: center;
+  align-items: center;
+  background-color: lightgray;
   border: 1px solid black;
+  border-radius: 5px;
  
 `;
 
+const MessageSlot = styled.div`
+ color: darkorange;
+ background-color: white;
+ padding: 0.5rem;
+  margin: auto; 
+`;
 const SquareSection = styled.section`
   display: grid;
   grid-template-columns: 1fr 1fr 1fr 1fr;
@@ -64,44 +84,121 @@ height: 600px;
   border-radius: 2px;
   justify-content: center;
 `;
-const LilSquare = styled.div`
-text-align: center;  
+const LilSquareContainer = styled.div`
+  text-align: center;  
+  padding: 0.1rem;
+  min-height: 100px;
+  height: 100%;
+  width: 100%;
+  border: 2px solid orange;
+  border-radius: 5px;
+
+`;
+
+const LilSquareBack = styled.p`
+color: orange;  
+text-align: left;  
+  padding: 0;
+  line-height: 0.77rem;
+  font-size: 0.77rem;
+`;
+
+const LilSquareFront = styled.div`
+  text-align: center;  
   padding: 2.7rem;
   min-height: 100px;
   height: 100%;
   width: 100%;
- border: 2px solid orange;
-  border-radius: 2px;
-   line-height: 3rem;
+  line-height: 3rem;
   font-size: 3rem;
+`;
+
+const LilTest = styled.div`
+  text-align: center;  
+  
+  min-height: 50px;
+  height: 100%;
+  width: 100%;
+  border: 2px solid orange;
+  border-radius: 2px;
+  line-height: 2rem;
+  font-size: 2rem;
 `;
 
 
 
+
+// console.log(generateSquareArray("memory", 4));
+
 export default function HomePage() {
+  const [squareState, setSquareState] = useState(initialState);
+  const [gameState, setGameState] = useState({ gameMode: "memory", progress: "noGame" });
+  const { gameMode, progress } = gameState;
+  const [message, setMessage] = useState("Welcome to  S Q U A R R E L");
+
+  function generateSquareArray(mode, root) {
+    const numberOfSquares = root ** 2;
+    const cardSets = numberOfSquares / 2;
+    const cardNumbers = [...Array(numberOfSquares + 1).keys()].slice(1);
+
+    const backLetters = "s Q U A R R E L "
+
+   const backSideArray = cardNumbers.map((number) => backLetters);
+      console.log(backSideArray);
+
+    const squareArray = cardNumbers.map((number) => {
+        // const timeStamp = Date.now();
+      
+      const front = Math.ceil(number / 2);
+      const squareObject = { id: number, mode, front, back: backSideArray, show: false };
+      return squareObject;
+    });
+    return squareArray;
+  }
+  
+
+
+  function handleStart() {
+   
+    setSquareState(generateSquareArray("memory", 4));
+    setGameState({...gameState, progress: "generated"})
+    setMessage(`Started a ${gameMode} game!`);
+  }
+
+
+
+ 
   return (
     <>
       <SquarrelTitle>🟧 S Q U A R R E L 🟧</SquarrelTitle>
-      <SquareSection>
-        <LilSquare>1</LilSquare>
-        <LilSquare>2</LilSquare>
-        <LilSquare>3</LilSquare>
-        <LilSquare>4</LilSquare>
-        <LilSquare>5</LilSquare>
-        <LilSquare>6</LilSquare>
-        <LilSquare>7</LilSquare>
-        <LilSquare>8</LilSquare>
-        <LilSquare>9</LilSquare>
-        <LilSquare>10</LilSquare>
-        <LilSquare>11</LilSquare>
-        <LilSquare>12</LilSquare>
-        <LilSquare>13</LilSquare>
-        <LilSquare>14</LilSquare>
-        <LilSquare>15</LilSquare>
-        <LilSquare>16</LilSquare>
+      <ButtonContainer><button onClick={handleStart}>START</button><MessageSlot>{message}</MessageSlot></ButtonContainer>
+     <SquareSection>
+        {progress === "generated" ? (squareState.map((square) => <LilSquareContainer key={square.id}><LilSquareBack>{square.back}</LilSquareBack></LilSquareContainer>)) : null}
+
     
 
       </SquareSection>
+      {/* <SquareSection>
+       <LilSquare>1</LilSquare>
+       <LilSquare>2</LilSquare>
+       <LilSquare>3</LilSquare>
+       <LilSquare>4</LilSquare>
+       <LilSquare>5</LilSquare>
+       <LilSquare>6</LilSquare>
+       <LilSquare>7</LilSquare>
+       <LilSquare>8</LilSquare>
+       <LilSquare>9</LilSquare>
+       <LilSquare>10</LilSquare>
+       <LilSquare>11</LilSquare>
+       <LilSquare>12</LilSquare>
+       <LilSquare>13</LilSquare>
+       <LilSquare>14</LilSquare>
+       <LilSquare>15</LilSquare>
+     <LilSquare>16</LilSquare> 
+     </SquareSection> */}
+      
+
+      <TestFlexContainer> </TestFlexContainer>
     </>
   );
 }
