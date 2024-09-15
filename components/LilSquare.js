57f449b8f53e829f24976c228905baea7d72ad42
Christof <userpattern@gmail.com>
2024-09-15 16:50:23 +0200

    workin on game logic

--- components/LilSquare.js
+++ components/LilSquare.js
@@ -9,11 +9,23 @@ const LilSquareContainer = styled.div`
   width: 100%;
   border: 2px solid orange;
   border-radius: 5px;
-
+    background-color: black;
 `;
+
+const LilSquareContainerWon = styled.div`
+  text-align: center;  
+  padding: 0.1rem;
+  min-height: 100px;
+  height: 100%;
+  width: 100%;
+  border: none;
+  border-radius: 5px;
+`;
+
 
 const LilSquareBack = styled.p`
 color: orange;  
+
 text-align: left;  
   padding: 0;
   line-height: 0.77rem;
@@ -21,8 +33,9 @@ text-align: left;
 `;
 
 const LilSquareFront = styled.div`
-  text-align: center;  
-  padding: 2.7rem;
+color: white;
+text-align: center;  
+padding: 2.7rem;
   min-height: 100px;
   height: 100%;
   width: 100%;
@@ -32,14 +45,19 @@ const LilSquareFront = styled.div`
 
 
 
-export default function LilSquare({id, front, back, show, onTurn}) {
+export default function LilSquare({ id, front, back, show, onTurn, won }) {
   
   function handleCardClick(id) {
     onTurn(id);
   }
+  if (won === true) {
+    return (<LilSquareContainerWon />)
+   
+  } else {
+    return (
+      <LilSquareContainer onClick={() => handleCardClick(id)}>{show ? <LilSquareFront>{front}</LilSquareFront> : <LilSquareBack>{back}</LilSquareBack>}</LilSquareContainer>)
   
-  
-  return (
-    
-    <LilSquareContainer onClick={()=>handleCardClick(id)}>{show? <LilSquareFront>{front}</LilSquareFront> : <LilSquareBack>{back}</LilSquareBack>}</LilSquareContainer>)
+  }
 }
+  
+ 
--- pages/index.js
+++ pages/index.js
@@ -38,6 +38,13 @@ const SquarrelTitle = styled.h1`
   margin:  2rem auto 2rem;
 `;
 
+const CardCount = styled.div`
+text-align: left; 
+color: darkorange;
+padding: 0.1rem;
+margin: 0.5rem;`
+  ;
+
 const TestFlexContainer = styled.div`
   display: flex;
   flex-direction: column;
@@ -62,8 +69,21 @@ const ButtonContainer = styled.div`
   background-color: lightgray;
   border: 1px solid black;
   border-radius: 5px;
- 
 `;
+
+const ConfirmMessage = styled.div`
+  display: flex;
+  flex-direction: row;
+  padding: 0.5rem;
+  margin: 1rem auto 1rem; 
+  min-height: 40px;
+  width: 50%;
+  align-content: center;
+  align-items: center;
+  border: 1px solid black;
+  border-radius: 5px;
+`;
+
 
 const MessageSlot = styled.div`
  color: darkorange;
@@ -89,11 +109,13 @@ height: 600px;
 
 export default function HomePage() {
   const [squareState, setSquareState] = useState(initialState);
-  const [gameState, setGameState] = useState({ gameMode: "memory", progress: "no game started yet", openCards: 0 });
-  const {gameMode, progress, openCards } = gameState;
+  const [gameState, setGameState] = useState({ gameMode: "memory", progress: "no game started yet", cardsShown: 0, score: 0, match: false });
+  const { gameMode, progress, cardsShown, score, match } = gameState;
+  const [openedCards, setOpenedCards] = useState([]);
+  const { card0, card1 } = openedCards;
   const [message, setMessage] = useState("Welcome to  S Q U A R R E L");
 
-  function generateSquareArray(mode, root) {
+  function generateSquareArray(mode, root, shuffle) {
     const numberOfSquares = root ** 2;
     const cardSets = numberOfSquares / 2;
     const cardNumbers = [...Array(numberOfSquares + 1).keys()].slice(1);
@@ -101,35 +123,101 @@ export default function HomePage() {
     const backLetters = "s Q U A R R E L "
     const backSideArray = cardNumbers.map((number) => backLetters);
      
-
     const squareArray = cardNumbers.map((number) => {
         // const timeStamp = Date.now();
       
       const front = Math.ceil(number / 2);
-      const squareObject = { id: number, mode, front, back: backSideArray, show: false };
+      const squareObject = { id: number, mode, front, back: backSideArray, show: false, won: false };
       return squareObject;
     });
-    return squareArray;
+    function shuffleArray(array) {
+      for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        const temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+      }
+      return array;
+    }
+    
+    return shuffle === true ? shuffleArray(squareArray) : squareArray;
   }
   
   function handleStart() {
-    setSquareState(generateSquareArray("memory", 4));
+    setSquareState(generateSquareArray("memory", 4, false));
     setGameState({ ...gameState, progress: "generated" })
 
     setMessage(`Started a ${gameMode} game!`);
   }
 
-  function handleReset() {
+  //need two kinds of resets
+  // total
+  function handletotalReset() {
     setSquareState(initialState);
     setGameState({ ...gameState, progress: "no game started yet" });
     setMessage("no game started yet");
     }
 
+  // restart
+  function handleRestart() {
+    setSquareState(squareState.map((card) => {
+      const newCard = { ...card, show: false, won: false }
+      return newCard;
+    }));
+    setGameState({ ...gameState, progress: "generated", cardsShown: 0, score: 0, match: false });
+    setOpenedCards([]);
+      setMessage("Click on a card to start!");
+      }
+  
+  
+  function handleMatch(card0, card1) {
+  
+      const postMatchSquareState = squareState.map((card) => {
+        if (card.front === card0.front) {
+          return {
+            ...card, won: true
+          }
+        
+        } else {
+          return card
+        }
+      });
+      setSquareState(postMatchSquareState);
+      setGameState({ ...gameState, score: gameState.score + 2 });
+    setMessage("You scored!");
+    }
+  
+  
+    function handelNoMatch(card0, card1) {
+       //close cards delay needed or dialog
+       const noMatchSquareState = squareState.map((card) => {
+        if (card.id === card0.id) {
+          return {
+            ...card, show: !card.show
+          }
+        
+       } else {
+        return card
+       } 
+      })
+        .map((card) => {
+          if (card.id === card1.id) {
+            return {
+              ...card, show: !card.show
+            }
+          
+         } else {
+          return card
+         } 
+        });
+      setSquareState(noMatchSquareState);
 
+    }
+  
   function turnAroundCard(id) {
-    const filteredSquareState =  squareState.filter((card) => card.show === true);
-    const openCardsCountBeforeClick = filteredSquareState.length;
-    console.log("openCardsCountBeforeClick: ",openCardsCountBeforeClick)
+    //following 2 lines obsolete now?
+    const filteredSquareStatePreClick =  squareState.filter((card) => card.show === true);
+    const openCardsCountBeforeClick = filteredSquareStatePreClick.length;
     const newSquareState = squareState.map((card) => {
       if (card.id === id) {
         return {
@@ -142,18 +230,42 @@ export default function HomePage() {
     });
     
     setSquareState(newSquareState);
-    const filteredSquareStatePostClick = newSquareState.filter((card) => card.show === true);
-    const openCardsCount = filteredSquareStatePostClick.length;
-    setGameState({ ...gameState, openCards: openCardsCount });
-    setMessage(`You turned card no. ${id}. Open CardCount (post click): ${openCardsCount}`);
+    const filteredSquareState = newSquareState.filter((card) => card.show === true);
+    const openCardsCount = filteredSquareState.length;
+    setGameState({ ...gameState, cardsShown: openCardsCount });
+    setMessage(`You turned card no. ${id}. Open CardCount: ${openCardsCount}`);
+    //check for too many cards not needed, instead check for match and autoclose
+    
+    if (openCardsCount <= 1) {
+      null
+    } else {
+      const card0 = filteredSquareState[0];
+      const card1 = filteredSquareState[1]; 
+      if (card0.front === card1.front) {
+        setMessage("The cards match, yeah!");
+        setGameState({ ...gameState, match: true });
+        setOpenedCards([card0, card1]);
+        console.log(card0);
+        console.log(card1);
+      } else {
+        setMessage("The cards do not match!");
+       
+
+      }
+      // card0.front === card1.front ? setMessage("The cards match, yeah!") : setMessage("The cards do not match!");
+      // make cards disappear and count points
+    }
+  
+
   }
  
   return (
     <>
       <SquarrelTitle>🟧 S Q U A R R E L 🟧</SquarrelTitle>
-      <ButtonContainer><button onClick={handleStart}>START</button><MessageSlot>{message}</MessageSlot><button onClick={handleReset}>RESET</button></ButtonContainer>
-     <SquareSection>
-        {progress === "generated" ? (squareState.map((square) => <LilSquare onTurn={turnAroundCard} key={square.id} id={square.id} front={square.front} back={square.back} show={square.show} />)) : null}
+      <ButtonContainer><button onClick={handleStart}>START</button><MessageSlot>{message}</MessageSlot><CardCount>Open: {cardsShown}  Score: {score} </CardCount>  <button onClick={handleRestart}>RESTART</button></ButtonContainer>
+     {match && <ConfirmMessage><button onClick={()=>handleMatch(openedCards[0], openedCards[1])}>Ok</button></ConfirmMessage>}
+      <SquareSection>
+        {progress === "generated" ? (squareState.map((square) => <LilSquare onTurn={turnAroundCard} key={square.id} id={square.id} front={square.front} back={square.back} show={square.show} won={square.won} />)) : null}
 
     
 
