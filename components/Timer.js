b774239072399f6f1e8165ea10bc1fbf763a285e
Christof <userpattern@gmail.com>
2024-10-02 09:58:07 +0200

    timer working, automatic  integration still to do

--- components/Timer.js
+++ components/Timer.js
@@ -16,80 +16,64 @@ padding: .5rem;
 `;
 export default function Timer() {
     const [timeTools, setTimeTools] = useState({ start: 0, running: false })
-    const { start, running, interval } = timeTools;
+    const { start, running, interval1, interval2 } = timeTools;
     const [timespan, setTimespan] = useState(0);
-    const [testN, setTestN] = useState(0);
+    const [hundredth, setHundredth] = useState(0);
 
-  
-    function startTiming() {
-        const first = Date.now();
-        const run = true;
-        setTimeTools({ start: first, running: run });
-        function updateTimespan() { 
-            const newTimespan = Math.round((Date.now() - first)/1000);
-            setTimespan(newTimespan);
-        }
-        console.log(running);
-        const nIntervId = setInterval(updateTimespan, 1000, timespan);
-        
-    }
 
     function advancedTiming(run) {
         const first = Date.now();
-        console.log(Math.round(first/1000))
+        
         if (run === true) {
             const running = true;
             setTimeTools({ start: first, running });
+
             function updateTimespan() {
-                const newTimespan = Math.round((Date.now() - first) / 1000);
-                setTimespan(newTimespan);
+                const newTimespan = (Date.now() - first) / 1000;
+                const roundedTime = Math.round(newTimespan);
+                setTimespan(roundedTime);
+                
+                function fakeHundredth() {
+                    const numbers = [...Array(100).keys()];
+                   numbers.forEach((number) => setTimeout(setHundredth, 10 * number, number));        
+                }
+                fakeHundredth();
             }
-            const nIntervId = setInterval(updateTimespan, 1000, timespan);
-            setTimeTools({ ...timeTools, interval: nIntervId })
+          
+            const newIntervalId = setInterval(updateTimespan, 1000);
+            setTimeTools({ ...timeTools, interval1: newIntervalId })
          
         } else { 
-            const last = Date.now();
-            console.log(Math.round(last/1000))
-            const timeInSeconds = Math.round((last - start) / 1000);
             setTimeTools({ ...timeTools, running: false });
-            console.log(timeInSeconds);
-            const nIntervId = interval;
-            clearInterval(nIntervId);
-            setTimespan(timeInSeconds);
-    }
+            const newIntervalId = interval1;
+            clearInterval(newIntervalId);
+        }
+      
     }
 
     
 
-    function stopTiming() {
-        const last = Date.now();
-        const timeInSeconds = Math.round((last - start) /1000);
-        setTimeTools({ ...timeTools, running: false });
-        
-        clearInterval(nIntervId);
-        setTimespan(timeInSeconds);
-        nIntervId = null;
-        console.log("stop");
+    function formatHundredth(number) {
+        const formattedNumber = number.toLocaleString('en-US', {
+            minimumIntegerDigits: 2,
+            useGrouping: false
+                    })
+        return formattedNumber;
     }
-
-    function ShowHundredth() {
-        let n = 0;
-        setInterval(()=>n++,500)
-        return (
-            <>
-                {n}
-            </>
-        )
-}
 
     return(
         <TimerSection>
             <SmallerHeadline> Timer</SmallerHeadline>
-            <TimeDisplay> {timespan}</TimeDisplay>
+            <TimeDisplay> {timespan},{formatHundredth(hundredth)}
+            </TimeDisplay>
             <ButtonContainer>
             <StandardButton onClick={()=>advancedTiming(true)}>Start</StandardButton>
             <StandardButton onClick={()=>advancedTiming(false)}>Stop</StandardButton>
-            <StandardButton onClick={()=>setTimespan(0) }>Reset</StandardButton>
+                <StandardButton onClick={() => {
+                    setTimespan(0);
+                    setHundredth(0);
+                }
+                 }>Reset</StandardButton>
             </ButtonContainer>
         </TimerSection>
     )
