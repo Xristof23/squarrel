8a56f52efb3239ecc9295beffd8cf610538fe6b2
Christof <userpattern@gmail.com>
2024-10-10 14:26:38 +0200

    new set images, windowwidth

--- pages/index.js
+++ pages/index.js
@@ -157,6 +157,32 @@ export default function HomePage() {
 }
  
   //  responsive
+
+
+    const isWindowClient = typeof window === "object";
+  
+    const [windowWidth, setWindowWidth] = useState(
+      isWindowClient ? window.innerWidth : undefined
+    );
+  
+    useEffect(() => {
+      
+      function setSize() {
+        setWindowWidth(window.innerWidth);
+      }
+      if (isWindowClient) {
+        //register the window resize listener
+        window.addEventListener("resize", setSize);
+  
+        //un-register the listener
+        return () => window.removeEventListener("resize", setSize);
+      }
+    }, [isWindowClient, setWindowWidth]);
+
+  console.log(windowWidth);
+   
+
+
   const cardSectionWidth = 800;
   const shiftRight = 100;
 
